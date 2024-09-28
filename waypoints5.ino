aafefa14352bc3fd962ec5dc1d919c728cdb5599
jez <77672808+cayleyflyer@users.noreply.github.com>
2024-09-28 19:59:33 +0100

    Add files via upload

---
+++ waypoints5.ino
@@ -0,0 +1,97 @@
+//GeoPosition currPos(53.897078, -0.166197);
+#include <Arduino.h>
+#include <FS.h>
+#include <SD.h>
+#include <SPI.h>
+#include <MicroNMEA.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SharpMem.h>
+#include "gnssmodule.h"
+#include "sharedspisdcard.h"
+#include "sharedspidisplay.h"
+#include "simpletile.h"
+#include "constgeoposition.h"
+#include "tileblockrenderer.h"
+#include "uirenderer.h"
+//#include "serialutils.h"
+#include "globalconfig.h"
+#include "interppositionprovider.h"
+#define DISPLAY_CS 2
+#define SDCARD_CS 2
+
+// Header of bin map file
+SimpleTile::Header header;
+
+uint16_t currHeading = 0;
+
+// Fixed position provider for debugging
+//GeoPosition currPos(53.897078, -0.166197);
+//ConstGeoPosition mockPosProvider(currPos, currHeading);
+
+SharedSPIDisplay display(DISPLAY_CS);
+SharedSPISDCard sdcard(SDCARD_CS);
+GNSSModule gnss(0);
+GPXTrack track;
+
+
+InterpPositionProvider ipos(&gnss, ((float)GNSS_MIN_UPDATE_TIME_MS) / ((float)TARGET_FRAME_TIME_MS));
+
+// Path to map-file on SD-card
+const char binary_path[] = "/map.bin";
+// Path to gpx-track file on SD-card
+const char gpx_path[] = "/track.gpx";
+
+
+
+void setup() {
+
+  
+
+  sleep(2);
+  SPI.setFrequency(SPI_FREQ);
+  SPI.begin();
+  Serial.begin(9600);
+  display.initialize();
+  display.setRotation(0);
+  gnss.initialize();
+  sdcard.initialize();
+  sdcard.setMapPath(binary_path);
+  sdcard.setGPXTrackInPath(gpx_path);
+  while (!sdcard.readHeader(header)) {
+    UIRENDERER.delay(100);
+  }
+  UIRENDERER.step();
+  if (sdcard.readGPX(header, track)) {
+    UIRENDERER.setGPXTrackIn(&track);
+    BOOTSCREEN.trackOK = true;
+  } else {
+    // Track could not be initialized. Setup continues.
+    BOOTSCREEN.trackOK = -1;
+    UIRENDERER.delay(500);
+  }
+  UIRENDERER.setHeader(&header);
+  UIRENDERER.setGNSS(&gnss);
+  if (!UIRENDERER.initializeMap(&sdcard)) {
+    // Map could not be initialized due to insufficient memory. Setup stops.
+    BOOTSCREEN.mapOK = -1;
+    while (1) {
+      UIRENDERER.delay(1000);
+    }
+  } else {
+    BOOTSCREEN.mapOK = true;
+  }
+  UIRENDERER.setPositionProvider(&ipos);
+
+  // Setup was successful. Render some more frames of the bootscreen to show it.
+  UIRENDERER.delay(500);
+
+  // Change to map
+  UIRENDERER.setScreen(&MAPSCREEN);
+}
+
+void loop() {
+
+UIRENDERER.step();
+
+}
+
