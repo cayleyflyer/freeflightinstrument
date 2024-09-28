ec2358bf60022265eb736928ae85413a97ebe0c8
jez <77672808+cayleyflyer@users.noreply.github.com>
2024-09-28 20:06:58 +0100

    Update gpxtrack.h

--- gpxtrack.h
+++ gpxtrack.h
@@ -14,15 +14,7 @@ public:
     int16_t* xList;
     int16_t* yList;
     uint64_t* tileIdList;
-    char* wplist; //char* wplist works,  char*wplist[8] and char wplist does not work
-    char* wplist1; //char* wplist works,  char*wplist[8] and char wplist does not work
-    char* wplist2; //char* wplist works,  char*wplist[8] and char wplist does not work
-    char* wplist3; //char* wplist works,  char*wplist[8] and char wplist does not work
-    char* wplist4; //char* wplist works,  char*wplist[8] and char wplist does not work
-    char* wplist5; //char* wplist works,  char*wplist[8] and char wplist does not work
-    char* wplist6; //char* wplist works,  char*wplist[8] and char wplist does not work
-    char* wplist7; //char* wplist works,  char*wplist[8] and char wplist does not work
-    char* wplist8; //char* wplist works,  char*wplist[8] and char wplist does not work
+    char** nameList;
     uint32_t numNodes;
     uint32_t nearestNodeId;
 };
