ffc335f7b581c9cd8946077d0066e7545bda6ffa
jez <77672808+cayleyflyer@users.noreply.github.com>
2025-05-24 21:27:04 +0100

    Delete screens.cpp

--- screens.cpp
+++
@@ -1,7 +0,0 @@
-#include "screens.h"
-
-BootScreen BOOTSCREEN;
-FixWaitingScreen FIXWAITINGSCREEN;
-MapScreen MAPSCREEN;
-MainScreen MAINSCREEN;  //Screen2 basic flight data
-ScreenThree SCREENTHREE;//screen3
